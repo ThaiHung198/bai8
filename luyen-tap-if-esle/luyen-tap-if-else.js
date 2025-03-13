d5a5a08eeadbeeb1b9a4ea5a8dd2fb22e0a7d09d
thaihung198 <thaihung8503@gmail.com>
2025-03-13 11:24:27 +0700

    luyện tập if else

---
+++ luyen-tap-if-esle/luyen-tap-if-else.js
@@ -0,0 +1,7 @@
+let value=prompt("bạn có chắc chắn trang của Thái Hưng không?")
+    if(value=="tôi chắc chắn"){
+        alert("chuẩn rồi")
+    }
+    else {
+        alert("Nhầm mất rồi b êi")
+    }
