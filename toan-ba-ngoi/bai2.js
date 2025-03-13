77f89b9c360936247a5f110f47c87222efdb55c2
thaihung198 <thaihung8503@gmail.com>
2025-03-13 15:45:35 +0700

    toan-ba-ngoi

---
+++ toan-ba-ngoi/bai2.js
@@ -0,0 +1,10 @@
+let login = prompt("Enter:");
+if(login =='Employee' ){
+    alert('hello');
+} else if (login =='Director') {
+    alert('Greetings');
+} else if (login =='') {
+    alert('no login');
+} else  {
+    alert("")
+}
